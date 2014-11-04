bf0d930e93f356c7e9a31a7ebe3503f82af589d3
Tom Cowling <tom.cowling@gmail.com>
2014-11-04 11:30:09 +0000

    Log specifically to a /var/log directory

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,9 @@
 # == Class tomcat::install
 #
 class tomcat::install {
+
   package { $tomcat::package_name:
     ensure => present,
   }
-  file { 'Tomcat Upstart':
-    ensure  => file,
-    path    => '/etc/init/tomcat.conf',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => template('tomcat/etc/init/tomcat.conf'),
-    require => Package[$tomcat::package_name],
-  }
+
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -5,10 +5,22 @@
 #
 class tomcat::service {
 
+   file { 'tomcat conf':
+    ensure  => file,
+    path    => '/etc/init/tomcat.conf',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('tomcat/etc/init/tomcat.conf'),
+  }
+
+  
+
   service { $tomcat::service_name:
     ensure     => running,
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
   }
+
 }
